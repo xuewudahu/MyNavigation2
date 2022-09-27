d8e90e42a6c9e737151d35c7961e84084076024e
xuewudahu <1774515717@qq.com>
2022-09-27 09:41:10 +0800

    提交第一次

---
+++ app/src/main/java/com/study/mynavigation/MainActivity.java
@@ -0,0 +1,19 @@
+package com.study.mynavigation;
+
+import androidx.annotation.IdRes;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
